22789f55d1bcda02b1c039ba33abc1c4464cb872
xue <2723709118@qq.com>
2024-08-05 20:26:57 +0800

    会动的皮卡丘

--- js/main.js
+++ js/main.js
@@ -138,7 +138,7 @@
 * 接下来，画皮卡丘的小嘴巴
 */
 .upperLip{
-  height: 21px;
+  height: 22px;
   width: 80px;
   border: 3px solid black; 
   position: absolute;
@@ -148,14 +148,14 @@
 }
 .upperLip.left{
   right: 50%;
-  border-bottom-left-radius: 40px 21px;
+  border-bottom-left-radius: 40px 22px;
   border-top: none;
   border-right: none;
   transform: rotate(-20deg);
 }
 .upperLip.right{
   left: 50%;
-  border-bottom-right-radius: 40px 21px;
+  border-bottom-right-radius: 40px 22px;
   border-top: none;
   border-left: none;
   transform: rotate(20deg);
